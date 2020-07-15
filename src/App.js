d6994110b4c3ed2310185d932a8713963ffcaf44
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-07-14 18:16:38 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div>
+         <h1>Counter: 0</h1>
+         <div><button class="ui inverted green button">Increment</button></div>
+         <div><button class="ui inverted red button">Decrement</button></div>
+         
+         
+
+         </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
